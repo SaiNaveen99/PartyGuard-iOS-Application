9c79efef00a6c4c8c5077f1978603897e65bf754
Student <s525051@nw36118.nwmissouri.edu>
2016-09-29 16:11:40 -0500

    Integrating Basic Login View Web Service

--- Partyguard_iOS/BasicLoginViewController.swift
+++ Partyguard_iOS/BasicLoginViewController.swift
@@ -19,8 +19,62 @@ class BasicLoginViewController: UIViewController {
     @IBOutlet weak var PasswordTF: UITextField!
     
     
+    @IBOutlet weak var validationLBL: UILabel!
+    
+    
+    
     @IBAction func LoginButton(sender: AnyObject)
     {
+        
+        if(EmailTF.text == "" || PasswordTF.text == "")
+        {
+            
+            validationLBL.hidden = false
+            validationLBL.text = " Username and Password fields should not be Empty"
+            
+        }
+        else
+        {
+         
+            self.performSegueWithIdentifier("LoginSegue", sender: self)
+            let ivc:InfoViewController = InfoViewController()
+            
+            presentViewController(ivc, animated: true, completion: nil)
+            
+        let textdata = "username="+EmailTF.text!+"&password="+PasswordTF.text!+"grant_type=password"
+        
+        do {
+           
+            
+            let url = NSURL(string: "http://partyguardservices20160912122440.azurewebsites.net/token")!
+            let request = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            
+            // insert json data to the request
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = textdata.dataUsingEncoding(NSUTF8StringEncoding)
+            
+            
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data, response, error in
+                if error != nil{
+                    print("Error -> \(error)")
+                    return
+                }
+                
+                do {
+                    let result = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String:AnyObject]
+                    
+                    print("Result -> \(result)")
+                    
+                } catch {
+                    print("Error -> \(error)")
+                }
+            }
+            
+            task.resume()
+           
+        }
+        }
         
     }
     
@@ -38,6 +92,7 @@ class BasicLoginViewController: UIViewController {
     {
         TitleLBL.text = "PartyGuard"
         self.view.backgroundColor = UIColor.lightTextColor()
+        validationLBL.hidden = true
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }

32c4821d065ef5d851408e5a03b81370b5df7478
BashGuardians <bashguardians@bashguardianss-macbook-pro.local>
2016-10-21 13:25:23 -0500

    Created Basic User Profile Controller

---
+++ Partyguard_iOS/BasicUserProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BasicUserProfileViewController.swift
+//  Partyguard_iOS
+//
+//  Created by BashGuardians on 10/21/16.
+//  Copyright © 2016 BashGuardians. All rights reserved.
+//
+
+import UIKit
+
+class BasicUserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

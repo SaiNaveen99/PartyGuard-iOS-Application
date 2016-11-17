782fdda39e02545b6d67cfb8990bf8fa0cd3f8d3
Student <s525120@ol23206m.nwmissouri.edu>
2016-11-16 21:20:58 -0600

    Pg team sc

---
+++ Partyguard_iOS/MyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewCell.swift
+//  Partyguard_iOS
+//
+//  Created by Tankasala,Nandeesh on 11/16/16.
+//  Copyright © 2016 BashGuardians. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var guardname:UILabel!
+    @IBOutlet weak var guardimage:UIImageView!
+    @IBAction func profilebutton(sender:AnyObject)
+    {
+        
+    }
+}
